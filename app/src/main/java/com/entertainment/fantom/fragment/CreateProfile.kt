b66891375b9d5c960ccf3f6e3083fcc8e1dd4993
v0s0207 <vartika.sharma@walmart.com>
2021-10-12 12:33:38 +0530

    added fragments

--- app/src/main/java/com/entertainment/fantom/fragment/CreateProfile.kt
+++ app/src/main/java/com/entertainment/fantom/fragment/CreateProfile.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import butterknife.ButterKnife
 import com.entertainment.fantom.R
 
 private const val ARG_PARAM1 = "param1"
@@ -19,7 +20,7 @@ class CreateProfile : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,7 +34,10 @@ class CreateProfile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_profile, container, false)
+        val view:View = inflater.inflate(R.layout.fragment_create_profile, container, false)
+        retainInstance = true
+        ButterKnife.bind(this,view)
+        return view
     }
 
     companion object {
